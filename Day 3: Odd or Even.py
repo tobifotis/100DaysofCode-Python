dca66ab450883902c909577ee1515fb81330c6e2
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-05-11 22:36:19 -0500

    Update Day 3: Odd or Even.py

--- Day 3: Odd or Even.py
+++ Day 3: Odd or Even.py
@@ -1,8 +1,4 @@
-# Which number do you want to check?
 number = int(input())
-# 🚨 Don't change the code above 👆
-
-# Write your code below this line 👇
 
 if number % 2 != 0:
   print("This is an odd number.")
