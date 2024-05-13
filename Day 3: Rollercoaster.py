325ae10fff5fcf8d220c9c6c9581b629d8ba6a98
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-05-13 02:27:02 +0000

    Update Day 3: Rollercoaster

--- Day 3: Rollercoaster.py
+++ Day 3: Rollercoaster.py
@@ -22,8 +22,8 @@ if height > 120:
   wants_photo = input("Do you want a photo taken? Y or N. ")
   if wants_photo == "Y":
     bill += 3
-    
+
   print(f"Your final bill is ${bill}")
-  
+
 else:
   print("You are not tall enough to ride this rollercoaster!")
