b164345c00d57ab2df1f63956340eb48693b9de4
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-05-13 04:42:08 +0000

    Day 3 Project: Treasure Island

---
+++ Day 3 Project: Treasure Island.py
@@ -0,0 +1,42 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the \"Vibranium\".\n")
+choice1 = input('You are at a cross road, Where do you want to go? Type "left" or "right" \n').lower()
+
+if choice1 == "left":
+  choice2 = input('\nYou find yourself in front of a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input('\nYou arrive at Wakanda island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which colour do you choose? \n').lower()
+    if choice3 == "red":
+      print("You got burned by Fire. Game Over!")
+    elif choice3 == "blue":
+      print("You got eaten by beasts. Game Over!")
+    elif choice3 == "yellow":
+      print("\nYou have found the Vibranium. You Win!")
+    else: 
+      print("Game Over!")
+  else:
+    print("You got attacked by an Alligator. Game over!")
+else:
+  print("You fell into a well. Game over!")
