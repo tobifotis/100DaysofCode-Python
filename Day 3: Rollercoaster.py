c254dd4e8ed579729949a68d13167407b5dcbac4
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-05-12 19:06:52 -0500

    Update Day 3: Rollercoaster.py

--- Day 3: Rollercoaster.py
+++ Day 3: Rollercoaster.py
@@ -1,14 +1,26 @@
 print("Welcome to the rollercoaster!")
+
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height > 120:
-  print("You are tall enough to ride this rollercoaster!")
-  age = int(input("How old are you? "))
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
   if age < 12:
-    print("You have to pay $5")
+    bill = 5
+    print("Child tickets are $5.")
   elif age > 18:
-    print("You have to pay $12")
+    bill = 12
+    print("Adult tickets are $12.")
   else:
-    print("You have to pay $7")
+    bill = 7
+    print("Youth tickets are $7.")
+
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+    
+  print(f"Your final bill is ${bill}")
+  
 else:
   print("You are not tall enough to ride this rollercoaster!")
