572b6883613108a8da1ce4ce1fef3d51bff3d8db
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-05-12 21:25:02 -0500

    Update Day 3: Rollercoaster.py

--- Day 3: Rollercoaster.py
+++ Day 3: Rollercoaster.py
@@ -9,12 +9,15 @@ if height > 120:
   if age < 12:
     bill = 5
     print("Child tickets are $5.")
-  elif age > 18:
-    bill = 12
-    print("Adult tickets are $12.")
-  else:
+  elif age <= 18:
     bill = 7
     print("Youth tickets are $7.")
+  elif age >= 45 and age <= 55:
+    bill = 0
+    print("Everything is going to be ok. Have a free ride on us!")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
 
   wants_photo = input("Do you want a photo taken? Y or N. ")
   if wants_photo == "Y":
