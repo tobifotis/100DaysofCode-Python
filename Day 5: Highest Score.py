f749447cc75f7f430bd93dfc2351ecc2ee68e00c
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-05-14 14:41:13 +0000

    Day 5: Highest Score

---
+++ Day 5: Highest Score.py
@@ -0,0 +1,12 @@
+# Input a list of student scores
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+
+# Your code here
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
