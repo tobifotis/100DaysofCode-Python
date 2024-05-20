c036a4d3d480710b1d1f9732a05d7789b0686e61
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-05-20 19:51:07 +0000

    Day 8 Project: Caeser Cipher Final

--- Day 8 Project: Caeser Cipher Final.py
+++ Day 8 Project: Caeser Cipher Final.py
@@ -22,7 +22,7 @@ while not should_end:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
-
+  
   shift = shift % 26
 
   caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
@@ -31,6 +31,8 @@ while not should_end:
   if restart == "no":
     should_end = True
     print("Goodbye")
+
+
 
 
 
