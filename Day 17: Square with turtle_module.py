51f2d4c9b28a84cae512b49d4f8289c2a56f2738
Oluwatobi Emmanuel <136779793+tobifotis@users.noreply.github.com>
2024-06-06 22:43:17 +0000

    Day 17: Square with turtle_module

---
+++ Day 17: Square with turtle_module.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+bruno = Turtle()
+bruno.shape("turtle")
+bruno.color("royalblue4")
+
+for _ in range(4):
+    bruno.forward(140)
+    bruno.right(90)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
